4fa5044aeb6f7168c0b00fc53052c3cf83244918
Your Name <toomelaaimar@gmail.com>
2024-12-15 14:06:01 +0200

    Tasks 1-12 done

--- shortener_app/admin.py
+++ shortener_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import URLShortener, AliasedURL  # Import both models
 
-# Register your models here.
+# Register the models so they appear in the admin panel
+admin.site.register(URLShortener)
+admin.site.register(AliasedURL)
--- shortener_app/models.py
+++ shortener_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from shortener_app.utils import get_random_alias
+
 
 # Create your models here.
+class URLShortener(models.Model):
+    alias = models.CharField(max_length=8, unique=True)  # Alias column with a max length of 8, unique
+    original_url = models.URLField()  # URL column for the original URL
+
+
+ALIAS_LENGTH = 8
+
+class AliasedURL(models.Model):
+    alias = models.CharField(primary_key=True,
+                             max_length=ALIAS_LENGTH,
+                             default=get_random_alias)
+    url = models.URLField(blank = False)
+
+    def __str__(self):
+        return f"{self.alias} -> {self.url}"
