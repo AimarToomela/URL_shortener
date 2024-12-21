00f22e26984c2d6c33d1c375b0fa6ce571a34dc3
Your Name <toomelaaimar@gmail.com>
2024-12-21 09:38:36 +0200

    Task until 15

--- URL_shortener/urls.py
+++ URL_shortener/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from shortener_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<str:alias>/', views.redirect_view, name='redirect_view'),
+    path('', include('URL_shortener.urls')),
 ]
--- shortener_app/views.py
+++ shortener_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseNotFound
+from .models import AliasedURL
 
 # Create your views here.
+
+def redirect_view(request, alias):
+    url_mapping = get_object_or_404(AliasedURL, alias = alias)
+    return redirect(url_mapping.url)
